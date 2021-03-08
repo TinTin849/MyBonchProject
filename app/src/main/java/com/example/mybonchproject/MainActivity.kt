44dbb2111a18078f62b426b7110f24c34c96c326
TinTin <tintin200227@gmail.com>
2021-03-08 22:42:25 +0300

    clearing project

--- app/src/main/java/com/example/mybonchproject/MainActivity.kt
+++ app/src/main/java/com/example/mybonchproject/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.mybonchproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.OnClickAction
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onPush(onClickAction: OnClickAction){
     }
 }
